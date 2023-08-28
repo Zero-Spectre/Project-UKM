632d62dbffb08e2623f12fb5316ae736dcd3cd71
Rujak97 <134048440+Rujak97@users.noreply.github.com>
2023-08-28 23:50:08 +0700

    Add files via upload

---
+++ seal/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/**/*.{html,js}'],
+  theme: {
+      fontFamily:{
+        popins: ['Poppins', 'sans-serif'],
+        sacramento: ['Sacramento', 'cursive'],
+      },
+    extend: {},
+  },
+  plugins: [],
+}
+
